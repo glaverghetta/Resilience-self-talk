adf3a1ff4cccfbccdd6f3464527a1488f2bc441d
glaverghetta1 <glaverghetta@usf.edu>
2021-09-05 14:20:54 -0400

    Prompt script comments

--- Assets/Scripts/Prompts.cs
+++ Assets/Scripts/Prompts.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// This class handles the prompts that the player will be asked during the course of the game.
 public class Prompts : MonoBehaviour
 {
     public delegate void promptAnsweredDelegate();
-    public event promptAnsweredDelegate promptAnsweredEvent;  // Event called when the player clicks "Continue" after answering the prompt
+    public event promptAnsweredDelegate promptAnsweredEvent;  // Event called when the player clicks "Continue" after answering the prompt.
 
     private static readonly string[] allPrompts = { "I show kindness by: ",                                  // All prompts that may be asked in the game.
                                                  "I am strong because I: ",
@@ -20,14 +20,13 @@ public class Prompts : MonoBehaviour
                                                  "My greatest accomplishment is: ",
                                                  "I overcome adversity by: ",
                                                  "My favorite part of the day is: ",
-                                                 "Something that makes me laugh is: "
-                                                                                         };
+                                                 "Something that makes me laugh is: "    };
 
     private List<string> promptsRemaining;      // List containing the prompts that the user has not been asked yet. Will be repopulated with all prompts if the user is asked every prompt.
     private string currentPrompt;               // Prompt user is currently being asked.
 
-    public GameObject promptUI;
-    public Text promptText;
+    public GameObject promptUI;                 // GUI used to display prompts on screen.
+    public Text promptText;                     // Text object containing the prompt currently being asked.
 
     void Start()
     {
@@ -40,16 +39,20 @@ public class Prompts : MonoBehaviour
 
     private void PickRandomPrompt()
     {
+        // If all prompts have been asked, add all possible prompts back into promptsRemaining.
         if(promptsRemaining.Count == 0)
         {
             RepopulatePromptsRemaining();
         }
-        int promptIndex = Random.Range(0, promptsRemaining.Count - 1);
+
+        // Get and show random prompt from promptsRemaining.
+        int promptIndex = Random.Range(0, promptsRemaining.Count);
         currentPrompt = promptsRemaining[promptIndex];
         promptsRemaining.RemoveAt(promptIndex);
         ShowPrompt(currentPrompt);
     }
 
+    // Adds all possible prompts back into promptsRemaining.
     private void RepopulatePromptsRemaining()
     {
         foreach (string prompt in allPrompts)
@@ -58,12 +61,14 @@ public class Prompts : MonoBehaviour
         }
     }
 
+    // Enables the prompt GUI and sets its text to the supplied argument.
     private void ShowPrompt(string prompt)
     {
         promptUI.SetActive(true);
         promptText.text = currentPrompt;
     }
 
+    // Function to be called when the user clicks the "Continue" button.
     public void PromptAnswered()
     {
         HidePrompt();
@@ -73,9 +78,9 @@ public class Prompts : MonoBehaviour
         }
     }
 
+    // Disables the prompt GUI.
     private void HidePrompt()
     {
         promptUI.SetActive(false);
-
     }
 }
